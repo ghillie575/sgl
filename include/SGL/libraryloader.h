41d9bfb938d29afbd56ce3322c766a3a808293df
Mikhail Karakou <mikhail@openspm.org>
2025-01-22 08:46:06 +0100

    chnaged some libraryloader logic, added libinfo

--- include/SGL/libraryloader.h
+++ include/SGL/libraryloader.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <memory>
 #include <filesystem>
-#include <SGL/window.h> 
+#include <SGL/window.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
@@ -17,30 +17,49 @@
 
 namespace fs = std::filesystem;
 class Window;
+enum LibType{
+    OBJECT,
+    COMPONENT,
+    WINDOW,
+    GENERAL
+};
+class LibInfo{
+
+public:
+    LibInfo(const std::string& id, const std::string& info, LibType type) : id(id), info(info), type(type) {};
+    LibInfo() {};
+    std::string id;
+    std::string info;
+    LibType type;
+};
+class SGLLibrary{
+
+public:
+    SGLLibrary(const std::string& path) : path(path) {};
+    LibInfo info;
+    void open();
+    void close();
+    void* getFunction(const char* funcName);
+    std::string path;
+private:
+    void* handle;
+    Logger logger = Logger("LibraryLoader");
+};
 class LibraryLoader
 {
 public:
     explicit LibraryLoader(const std::string& directory);
 
     void loadLibraries(Window* window);
+    void loadLibraries(Window* window,std::string dirPath);
     void executeOnTypeRegister(Window* window);
 private:
-    struct DynamicLibrary
-    {
-        std::string path;
-        void* handle;
-
-        explicit DynamicLibrary(const std::string& libPath);
-        ~DynamicLibrary();
-        void* getFunction(const char* funcName);
-    };
-
     std::string libraryDirectory;
     Logger logger;
-    std::vector<std::unique_ptr<DynamicLibrary>> libraries;
-
-    void loadLibrary(const std::string& libPath);
+    std::vector<std::unique_ptr<SGLLibrary>> libraries;
     
+    void loadLibrary(const std::string& libPath);
+
     bool isLibraryFile(const fs::path& path);
 };
 
